b2df4526dcd9604ca4e926806f72edf3b2754ed7
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-09-27 00:14:29 +0500

    fist commit from kq hostel
    abc desc

---
+++ Ch9-P5-GenericPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9_P5_GenericPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
