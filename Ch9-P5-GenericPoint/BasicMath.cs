08e6a05e1c6be7fc8a851b8f4e86c234ce994afe
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-09-28 00:34:05 +0500

    Added code for the last topic " Lack of Operator Constraints "

---
+++ Ch9-P5-GenericPoint/BasicMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9_P5_GenericPoint
+{
+    //class BasicMath<T> where T : operator +
+    class BasicMath<T>
+    {
+        //public T Add(T arg1, T arg2)
+        //{
+        //    return arg1 + arg2;
+        //}
+        //public T Subtract(T arg1, T arg2)
+        //{
+        //    return arg1 - arg2;
+        //}
+        //public T Multiply(T arg1, T arg2)
+        //{
+        //    return arg1 * arg2;
+        //}
+        //public T Divide(T arg1, T arg2)
+        //{
+        //    return arg1 / arg2;
+        //}
+    }
+}
