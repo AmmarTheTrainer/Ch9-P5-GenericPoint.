7da10c3a721c0280fe0f07566f7ebbf4421bc002
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-09-27 15:23:01 +0500

    dsfs

---
+++ Ch9-P5-GenericPoint/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9_P5_GenericPoint
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person() { }
+        public Person(string firstName, string lastName, int age)
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return $" [ Name: {FirstName} {LastName}, Age: {Age} ] ";
+        }
+    }
+}
